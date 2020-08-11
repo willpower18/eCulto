a70adc2679dc1594b213f8b6304ac9dcc5e653e7
willpower18 <willcpower@gmail.com>
2020-08-11 11:14:47 -0300

    inicio-participacao

--- src/pages/Participacao/index.js
+++ src/pages/Participacao/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { SafeAreaView, View, Text } from 'react-native';
+import { FontAwesome5 } from '@expo/vector-icons';
 import styles from './styles';
 
 export default function Participacao(){
     return(
-        <View>
-            <Text>Participacao</Text>
-        </View>
+        <SafeAreaView style={styles.container}>
+            <View style={styles.header}>
+                <Text style={styles.headerBrand}>Igreja Batista Betel</Text>
+                <Text style={styles.headerText}></Text>
+                <FontAwesome5 name="church" size={30} color="#8b57c9" />
+            </View>
+        </SafeAreaView>
     );
 }
