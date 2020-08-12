b1951147eebde35685291a6a98b28c2ee73baf2e
Wiliam Paulino <willjcpower@gmail.com>
2020-08-12 13:30:20 -0300

    consumo api 80%

--- src/pages/Cultos/index.js
+++ src/pages/Cultos/index.js
@@ -1,18 +1,19 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { SafeAreaView, View, Text, FlatList } from 'react-native';
 import { FontAwesome5 } from '@expo/vector-icons';
 import { ProgressBar, Button } from 'react-native-paper';
 import { useNavigation, useRoute } from '@react-navigation/native';
+import moment from 'moment';
 import api from '../../services/api';
 import styles from './styles';
 
-export default function Cultos(){
+export default function Cultos() {
     //Navegacao
     const navigation = useNavigation();
     const route = useRoute();
 
-    function navigateToParticipacao(idCulto){
-        navigation.navigate('Participacao',{idCulto});
+    function navigateToParticipacao(idCulto) {
+        navigation.navigate('Participacao', { idCulto, idIgreja: nomeIgreja });
     }
 
     //Estado da Aplicacao
@@ -20,12 +21,28 @@ export default function Cultos(){
     const [nomeIgreja, setNomeIgreja] = useState('');
     const [cultos, setCultos] = useState([]);
     const [capacidade, setCapacidade] = useState(0);
+
     //Funcoes
-    async function getData(){
+    async function getData() {
         const response = await api.get(`api/Igrejas/${idIgreja}?key=AIzaSyBuDB2x3H88svwDRtqC8L7JpXxuG4b2NAY`);
         setCapacidade(response.data.capacidade);
         setNomeIgreja(response.data.nomeIgreja);
-        setCultos(response.data.cultos);
+        const arrCultos = response.data.cultos;
+        const arrECultos = [];
+        for (let counter = 0; counter < arrCultos.length; counter++) {
+            let qtdAtual = parseFloat(arrCultos[counter].lotacao);
+            let qtdMaxima = parseFloat(response.data.capacidade);
+            let porcentagem = qtdAtual / qtdMaxima;
+            let eCulto = {
+                idCulto: arrCultos[counter].idCulto,
+                nome: arrCultos[counter].nome,
+                dataHora: arrCultos[counter].dataHora,
+                preletor: arrCultos[counter].preletor,
+                lotacao: porcentagem
+            }
+            arrECultos.push(eCulto);
+        }
+        setCultos(arrECultos);
     }
 
     useEffect(() => {
@@ -35,47 +52,76 @@ export default function Cultos(){
     }, []);
 
     //Retorno
-    return(
+    return (
         <SafeAreaView style={styles.container}>
-        <View style={styles.header}>
-            <Text style={styles.headerBrand}>{nomeIgreja}</Text>
-            <Text style={styles.headerText}></Text>
-            <FontAwesome5 name="church" size={30} color="#8b57c9" />
-        </View>
-        <View style={styles.header2}>
-            <Text style={styles.about}>Escolha um horário de culto disponível abaixo e confirme sua presença</Text>
-        </View>
-        <FlatList 
-            data={cultos}
-            keyExtractor={culto => String(culto.idCulto)}
-            showsVerticalScrollIndicator={false}
-            renderItem={({ item: culto }) => (
-                <View style={styles.dashBoard2}>
-                    <View style={styles.dashBoardHeader}>
-                        <Text style={styles.titleDash}>{culto.nome}</Text>
-                        <Text style={styles.titleDash}></Text>
-                        <FontAwesome5 name="clock" size={20} color="#7f7f7f" />
-                    </View>
-                    <View style={styles.currency}>
-                        <Text style={styles.fontCurrency}>{culto.preletor}</Text>
-                    </View>
-                    <View style={styles.currency}>
-                        <Text style={styles.fontCurrency}>{culto.dataHora}</Text>
-                    </View>
-                    <View style={styles.progressBar}>
-                        <ProgressBar progress={0.5} color='#8b57c9' />
-                    </View>
-                    <View style={styles.progressBar}>
-                        <Button icon="check" labelStyle={{ color: '#fff' }} mode="contained" color='#95c957' onPress={() => navigateToParticipacao(culto.idCulto)}>
-                            Quero Participar
-                        </Button>
-                    </View>
-                </View>
-            )}
-        />
-        <View>
-            <Text style={styles.wb}><FontAwesome5 name="copyright" size={11} color="8b57c9"/> WB Systems - Soluções em Informática</Text>
-        </View>
-    </SafeAreaView>
+            <View style={styles.header}>
+                <Text style={styles.headerBrand}>{nomeIgreja}</Text>
+                <Text style={styles.headerText}></Text>
+                <FontAwesome5 name="church" size={30} color="#8b57c9" />
+            </View>
+            <View style={styles.header2}>
+                <Text style={styles.about}>Escolha um horário de culto disponível abaixo e confirme sua presença</Text>
+            </View>
+            <FlatList
+                data={cultos}
+                keyExtractor={culto => String(culto.idCulto)}
+                showsVerticalScrollIndicator={false}
+                renderItem={({ item: culto }) => {
+                    if (culto.lotacao == 1) {
+                        return (
+                            <View style={styles.dashBoard2}>
+                                <View style={styles.dashBoardHeader}>
+                                    <Text style={styles.titleDash}>{culto.nome}</Text>
+                                    <Text style={styles.titleDash}></Text>
+                                    <FontAwesome5 name="clock" size={20} color="#7f7f7f" />
+                                </View>
+                                <View style={styles.currency}>
+                                    <Text style={styles.fontCurrency}>{culto.preletor}</Text>
+                                </View>
+                                <View style={styles.currency}>
+                                    <Text style={styles.fontCurrency}>{moment(culto.dataHora).format("DD/MM/YYYY HH:mm")}</Text>
+                                </View>
+                                <View style={styles.progressBar}>
+                                    <ProgressBar progress={culto.lotacao} color='#8b57c9' />
+                                </View>
+                                <View style={styles.progressBar}>
+                                    <Button icon="check" disabled={true} labelStyle={{ color: '#fff' }} mode="contained" color='#95c957' onPress={() => navigateToParticipacao(culto.idCulto)}>
+                                        Quero Participar
+                                    </Button>
+                                </View>
+                            </View>
+                        );
+                    }
+                    else {
+                        return (
+                            <View style={styles.dashBoard2}>
+                                <View style={styles.dashBoardHeader}>
+                                    <Text style={styles.titleDash}>{culto.nome}</Text>
+                                    <Text style={styles.titleDash}></Text>
+                                    <FontAwesome5 name="clock" size={20} color="#7f7f7f" />
+                                </View>
+                                <View style={styles.currency}>
+                                    <Text style={styles.fontCurrency}>{culto.preletor}</Text>
+                                </View>
+                                <View style={styles.currency}>
+                                    <Text style={styles.fontCurrency}>{moment(culto.dataHora).format("DD/MM/YYYY HH:mm")}</Text>
+                                </View>
+                                <View style={styles.progressBar}>
+                                    <ProgressBar progress={culto.lotacao} color='#8b57c9' />
+                                </View>
+                                <View style={styles.progressBar}>
+                                    <Button icon="check" labelStyle={{ color: '#fff' }} mode="contained" color='#95c957' onPress={() => navigateToParticipacao(culto.idCulto)}>
+                                        Quero Participar
+                                    </Button>
+                                </View>
+                            </View>
+                        );
+                    }
+                }}
+            />
+            <View>
+                <Text style={styles.wb}><FontAwesome5 name="copyright" size={11} color="8b57c9" /> WB Systems - Soluções em Informática</Text>
+            </View>
+        </SafeAreaView>
     );
 }
--- src/pages/Participacao/index.js
+++ src/pages/Participacao/index.js
@@ -1,24 +1,77 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { SafeAreaView, View, Text, TextInput } from 'react-native';
 import { Button } from 'react-native-paper';
 import { TextInputMask } from 'react-native-masked-text'
 import { FontAwesome5 } from '@expo/vector-icons';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import moment from 'moment';
+import api from '../../services/api';
 import styles from './styles';
 
 export default function Participacao() {
+    //Navegação
+    const navigation = useNavigation();
+    const route = useRoute();
+
+    //Estado da Aplicação
+    const [idCulto, setIdCulto] = useState(route.params.idCulto);
+    const [nomeIgreja, setNomeIgreja] = useState(route.params.idIgreja);
+    const [nomeCulto, setNomeCulto] = useState('');
+    const [horaCulto, setHoraCulto] = useState('');
     const [nome, setNome] = useState('');
     const [telefone, setTelefone] = useState('');
     const [qtdCriancas, setqtdCriancas] = useState('');
 
+    //Funcoes
+    async function handleSubmit() {
+        const data = {
+            key: "AIzaSyBuDB2x3H88svwDRtqC8L7JpXxuG4b2NAY",
+            participacao: {
+                IdParticipacao: 0,
+                IdCulto: idCulto,
+                Nome: nome,
+                Telefone: telefone,
+                ChaveApp: "-",
+                QtdCriancas: qtdCriancas,
+                QtdAdultos: 1,
+                Confirmado: 0
+            }
+        };
+        console.log(data);
+        /* const response = await api.post("api/participacao", data);
+         if (response.status == 200) {
+             alert('Presença Confirmada com Sucesso!');
+             setNome('');
+             setTelefone('');
+             setqtdCriancas('');
+         }
+         else {
+             alert('Não foi possível confirmar, tente novamente mais tarde.');
+         }*/
+    }
+
+    async function getData() {
+        const response = await api.get(`api/participacao/${idCulto}?key=AIzaSyBuDB2x3H88svwDRtqC8L7JpXxuG4b2NAY`);
+        setNomeCulto(response.data.nome);
+        setHoraCulto(response.data.dataHora);
+    }
+
+    useEffect(() => {
+        const loadData = navigation.addListener('focus', () => {
+            getData();
+        });
+    }, []);
+
+    //Retorno
     return (
         <SafeAreaView style={styles.container}>
             <View style={styles.header}>
-                <Text style={styles.headerBrand}>Igreja Batista Betel</Text>
+                <Text style={styles.headerBrand}>{nomeIgreja}</Text>
                 <Text style={styles.headerText}></Text>
                 <FontAwesome5 name="user-plus" size={30} color="#8b57c9" />
             </View>
             <View style={styles.header}>
-                <Text style={styles.headerText}>Culto de Celebração 12/08/2020 19:30</Text>
+                <Text style={styles.headerText}>{nomeCulto} às {moment(horaCulto).format("DD/MM/YYYY HH:mm")}</Text>
             </View>
             <View style={styles.dashBoard}>
                 <Text style={styles.headerText}>Preencha os dados abaixo:</Text>
@@ -54,8 +107,8 @@ export default function Participacao() {
                     autoCompleteType={"off"}
                     onChangeText={text => setqtdCriancas(text)}
                 />
-                <Text style={{ fontSize: 12, marginBottom: 5 }}>* Quantidade de Crianças Acima de 12 anos</Text>
-                <Button icon="check" labelStyle={{ color: '#fff' }} mode="contained" color='#95c957' onPress={() => { }}>
+                <Text style={{ fontSize: 12, marginBottom: 5 }}>* Quantidade de Crianças Acima de 7 anos</Text>
+                <Button icon="check" labelStyle={{ color: '#fff' }} mode="contained" color='#95c957' onPress={() => handleSubmit()}>
                     Confirmar
                 </Button>
             </View>
